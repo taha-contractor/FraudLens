13675b0d497c3f4bea098ff47b41f9ecd48a99c6
Taha Contractor <tasaif265@gmail.com>
2026-08-14 00:49:28 +0530

    feat: Connect MongoDB Database

--- server/server.js
+++ server/server.js
@@ -1,6 +1,10 @@
-import app from './app.js';
-const PORT = 5000;
+import app from './src/app.js';
+import connectDB from './src/config/database.js';
+import 'dotenv/config';
 
+const PORT = process.env.PORT || 5000;
+
+await connectDB();
 
 app.listen(PORT, () => {
     console.log(`FraudLens server running on http://localhost:${PORT}`);
