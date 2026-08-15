ee8c993ea2d5245fcf571ad88fbd1968b3097126
Taha Contractor <tasaif265@gmail.com>
2026-08-16 01:48:12 +0530

    feat: add transaction detail API

--- server/src/controllers/tracsaction.controller.js
+++ server/src/controllers/tracsaction.controller.js
@@ -35,3 +35,28 @@ export const getTransaction = async (req, res) => {
         });
     }
 }
+
+export const getTransactionById = async (req, res) => {
+    try {
+        const { id } = req.params;
+        const transaction = await Transaction.findOne({ transactionId: id });
+
+        if (!transaction) {
+            return res.status(404).json({
+                success: false,
+                message: "Transaction not found"
+            });
+        }
+
+        res.status(200).json({
+            success: true,
+            data: transaction
+        });
+    } catch (error) {
+        res.status(500).json({
+            success: false,
+            message: "Failed to fetch transaction",
+            error: error.message
+        });
+    }
+}
--- server/src/routes/transaction.routes.js
+++ server/src/routes/transaction.routes.js
@@ -1,8 +1,13 @@
 import express from "express";
-import {createTransaction, getTransaction} from "../controllers/tracsaction.controller.js";
+import {
+    createTransaction,
+    getTransaction,
+    getTransactionById
+} from "../controllers/tracsaction.controller.js";
 const router = express.Router();
 
 router.post("/", createTransaction);
 router.get("/", getTransaction);
+router.get("/:id", getTransactionById);
 
 export default router;
